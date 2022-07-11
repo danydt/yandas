77b6a8f712b18e61b9923b6a9ccf1ed16e9eb1d9
ngdaniel <danyditsia@gmail.com>
2022-07-11 16:08:28 +0100

    set this

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::redirect('/', '/login');
+Route::redirect('/register', '/login');
 
 Route::middleware('auth')->group(function() {
 
