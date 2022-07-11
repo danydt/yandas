f25985e74b594c8ae166ce9569c274b9e631b3e4
ngdaniel <danyditsia@gmail.com>
2022-07-11 13:38:30 +0100

    fix register response

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -63,6 +63,10 @@ class AuthController extends BaseController
 
         $userAuth = User::create($input);
 
+        Profile::updateOrInsert(
+            ['user_id' => $userAuth->id,],
+        );
+
         $user = User::join('profiles', 'users.id', '=', 'profiles.user_id')
                         ->select('users.*', 'profiles.photo', 'profiles.genre', 'profiles.phone_number', 'profiles.birthday')
                         ->where('users.id', $userAuth->id)
