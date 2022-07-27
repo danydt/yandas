2ac602621ab428e9df40353b29456bc7e80217de
ngdaniel <danyditsia@gmail.com>
2022-07-27 04:08:31 +0100

    add soft deletes oders

--- database/migrations/2022_07_27_021840_add_column_soft_deletes_tables_orders.php
+++ database/migrations/2022_07_27_021840_add_column_soft_deletes_tables_orders.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnSoftDeletesTablesOrderDetails extends Migration
+class AddColumnSoftDeletesTablesOrders extends Migration
 {
     /**
      * Run the migrations.
